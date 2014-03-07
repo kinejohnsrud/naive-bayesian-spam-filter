faa6cb9e9e281e2f71a5712d5351a700e0399cc1
Kine Johnsrud <kinejohnsrud@gmail.com>
2014-03-07 15:02:04 -0800

    Makes Word-class and enters words in HashMap<String, Word>

--- spam-filter/src/bayes/Bayes.java
+++ spam-filter/src/bayes/Bayes.java
@@ -4,37 +4,68 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class Bayes {
 	ArrayList<String> all = new ArrayList<String>();
 	ArrayList<String> spamList = new ArrayList<String>();
 	ArrayList<String> hamList = new ArrayList<String>();
+	HashMap<String, Word> words = new HashMap<String,Word>();
 	
 	
 	public static void main(String[] args) {
 		Bayes run = new Bayes();
-		
 		try {
 			run.testFileReader("/Users/Kine/Documents/GitHub/naive-bayesian-spam-filter/spam-filter/src/train.txt");
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		
-		
+		}		
 	}
 	
 	public void testFileReader(String input) throws IOException{
-		
+		int totalSpamCount = 0;
+		int totalHamCount = 0;
 		BufferedReader in = new BufferedReader(new FileReader(input));
 		String line = in.readLine();
-		
-		if(line.substring(0,line.indexOf(' ')).equals("ham")){
-			hamList.add(line.substring(line.indexOf(' ')+1));
+		while (line != null){
+			if (!line.equals("")){
+				String type = line.split("\t")[0];
+//				System.out.println("Type: " + type);
+				String sms = line.split("\t")[1];
+//				System.out.println("SMS: " + sms);
+				
+				for (String word : sms.split(" ")){
+					word = word.replaceAll("\\W", "");
+					word = word.toLowerCase();
+					Word w = null;
+					if(words.containsKey(word)){
+						w = (Word) words.get(word);
+					}
+					else {
+						w = new Word(word);
+						words.put(word,w);
+					}
+					if(type.equals("ham")){
+						w.countHam();
+						totalHamCount++;
+					}
+					else if(type.equals("spam")){
+						w.countSpam();
+						totalSpamCount++;
+					}		
+				}
+			}
+			line = in.readLine();	
 		}
-		else if(line.substring(0,line.indexOf(' ')).equals("spam")){
-			spamList.add(line.substring(line.indexOf(' ')+1));
-		}
-		
 		in.close();
+		
+		for (String key : words.keySet()) {
+		    words.get(key).calculateProbability(totalSpamCount, totalHamCount);
+		    System.out.println(words.get(key).getProbability());
+		}
+
+		
 	}
 }
--- spam-filter/src/bayes/Word.java
+++ spam-filter/src/bayes/Word.java
@@ -1,5 +1,56 @@
 package bayes;
 
 public class Word {
+	private String word;	//the word itself
+	private int spamCount;	//number of this words appearances in spam messages
+	private int hamCount;	//number of this words appearances in ham messages
+	private float spamRate;	//spamCount divided by total spam count
+	private float hamRate;	//hamCount divided by total ham count
+	private float probOfSpam;	//probability of word being spam
 	
+	public Word(String word){
+		this.word = word;
+		spamCount = 0;
+		hamCount = 0;
+		spamRate = 0.0f;
+		hamRate = 0.0f;
+		probOfSpam = 0.0f;	
+	}
+	
+	public void countSpam(){
+		spamCount++;
+	}
+	
+	public void countHam(){
+		hamCount++;
+	}
+	
+	//calculates the probability of spam, 
+	//and gives the smallest and biggest probabilities more precedence
+	public void calculateProbability(int totSpam, int totHam){
+		spamRate = spamCount / (float) totSpam;
+		hamRate = hamCount / (float) totHam;
+		
+		if(spamRate + hamRate > 0){
+			probOfSpam = spamRate / (spamRate + hamRate);
+		}
+		if(probOfSpam < 0.01f){
+			probOfSpam = 0.01f;
+		}
+		else if(probOfSpam > 0.99f){
+			probOfSpam = 0.99f;
+		}
+	}
+	
+	public float getProbability(){
+		return this.probOfSpam;
+	}
+	
+	
+	//The interesting rate is how far the probability is from 0.5 i.e. 50/50 chance.
+	public float interestingRate(){
+		return Math.abs(0.5f - probOfSpam);
+	}
+	
+
 }
